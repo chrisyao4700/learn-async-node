d1a38c1d0190a1933e5675e5de8f56dfd4ffc951
Yuan Yao <yuanyao@Yuans-MacBook-Pro-7.local>
2019-03-17 18:16:59 -0700

    REMOVE NODE_MODULES AND REMOVE ACTION

--- action/calculator.action.js
+++ action/calculator.action.js
@@ -37,25 +37,6 @@ class CalculatorAction {
         }
     }
 
-    static calculateNumberWithLongTime(operator, fir_num, sec_num, handler) {
-
-        const first_num = Number(fir_num);
-        const second_num = Number(sec_num);
-
-        if (!first_num && first_num !== 0) return handler('FIRST NUMBER IS NOT A VALID_NUMBER');
-        if (!second_num && second_num !== 0) return handler('SECOND NUMBER IS NOT A VALID_NUMBER');
-
-        let result = NaN;
-        if (operator === '+') result = first_num + second_num;
-        if (operator === '-') result = first_num - second_num;
-        if (operator === '*') result = first_num * second_num;
-        if (operator === '/') result = first_num / second_num;
-        if (operator === '^') result = Math.pow(first_num, second_num);
-        if (operator === '%') result = first_num % second_num;
-
-        handler(null, {first_num, second_num, result});
-    }
-
 }
 
 module.exports = CalculatorAction;
